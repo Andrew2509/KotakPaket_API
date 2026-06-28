3f6b8cb2feefbe928ab1f92448fc374ef2c72c27
Andrew <noreply@github.com>
2026-06-28 20:18:51 +0700

    feat: add temporary /link-storage route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/link-storage', function () {
+    $target = storage_path('app/public');
+    $link = public_path('storage');
+
+    if (file_exists($link)) {
+        return 'The "public/storage" directory already exists.';
+    }
+
+    if (symlink($target, $link)) {
+        return 'Symlink created successfully!';
+    }
+
+    return 'Failed to create symlink.';
 });
